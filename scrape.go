0f35dbd939543c81838c847176a0f662dfa7d2e5
alannanoguchi <alanna.noguchi@gmail.com>
2021-02-24 02:01:17 -0800

    adding individual data

--- scrape.go
+++ scrape.go
@@ -3,39 +3,50 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 
 	"log"
 
 	"github.com/gocolly/colly"
 )
 
-type restaurant struct {
+type Restaurant struct {
 	Name    string
+	Photo   string
 	Cuisine string
-	Rating  string
+	// Location string
+	Summary string
 }
 
-// main() contains code adapted from example found in Colly's docs:
-// http://go-colly.org/docs/examples/basic/
 func main() {
+
 	selector := "body > table:nth-child(7) > tbody > tr > td:nth-child(1) > table:nth-child(17) > tbody > tr > td > table > tbody" // grabs just the body
 
-	// Instantiate default collector
-	c := colly.NewCollector()
+	c := colly.NewCollector(
+		colly.AllowedDomains("zabihah.com", "www.zabihah.com"),
+	)
 
-	// On every a element which has href attribute call callback
+	infoCollector := c.Clone()
+
 	c.OnHTML(selector, func(e *colly.HTMLElement) {
+		restaurantUrl := e.ChildText("div.titleBS > a")
+		restaurantUrl = e.Request.AbsoluteURL(restaurantUrl)
+		infoCollector.Visit(restaurantUrl)
 
-		tmpRestaurant := restaurant{}
-		tmpRestaurant.Name = e.ChildText("div.titleBS > a")
-		tmpRestaurant.Cuisine = e.ChildText("#alertBox2 > div")
-		tmpRestaurant.Rating = e.ChildText("#badge_score")
-
-		// link := e.Attr("href")
 		e.ForEach("tr > td:nth-child(3)", func(_ int, h *colly.HTMLElement) { // for loop, to get each individual restaurant
 			link := h.ChildAttr("a", "href")
 			fmt.Printf("Link found: -> %s\n", link)
 		})
+	})
+
+	infoCollector.OnHTML("body > table:nth-child(7) > tbody > tr > td", func(e *colly.HTMLElement) {
+		tmpRestaurant := Restaurant{}
+		tmpRestaurant.Name = e.ChildText("body > table:nth-child(7) > tbody > tr > td:nth-child(1) > table:nth-child(7) > tbody > tr > td:nth-child(1)")
+		tmpRestaurant.Photo = e.ChildAttr("body > table:nth-child(7) > tbody > tr > td:nth-child(1) > table:nth-child(9) > tbody > tr:nth-child(2) > td > table > tbody > tr > td:nth-child(1) > img", "src")
+		tmpRestaurant.Cuisine = e.ChildText("#alertBox2 > div > b")
+
+		tmpRestaurant.Summary = strings.TrimSpace(e.ChildText("#alertBox2"))
+
 		js, err := json.MarshalIndent(tmpRestaurant, "", "    ")
 		if err != nil {
 			log.Fatal(err)
@@ -43,23 +54,16 @@ func main() {
 		fmt.Println(string(js))
 	})
 
-	// Before making a request print "Visiting ..."
 	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL.String())
+		fmt.Println("Visiting: ", r.URL.String())
 	})
 
-	//Start scraping https://www.zabihah.com/sub/United-States/California/Sacramento/QE8Gfd3yNC
-	c.Visit("https://www.zabihah.com/sub/United-States/California/Sacramento/QE8Gfd3yNC")
+	infoCollector.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting Restaurant URL: ", r.URL.String())
+	})
 
-	// // Create output file
-	// outputFile, err := os.Create("output.html")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// //Copy data from HTTP response to file
-	// _, err = io.Copy(outputFile, link.Body)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	// uncomment below line if you enable Async mode
+	// c.Wait()
+	startUrl := fmt.Sprintf("https://www.zabihah.com/sub/United-States/California/Sacramento/QE8Gfd3yNC")
+	c.Visit(startUrl)
 }
